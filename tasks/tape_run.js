7fa4b36faf3222699e48ab6d11274517bba2b889
Matias De Carli <matiasdecarli@gmail.com>
2015-06-09 15:59:22 -0300

    fail task when theres failed tests

--- tasks/tape_run.js
+++ tasks/tape_run.js
@@ -12,16 +12,18 @@ var run = require('tape-run');
 var browserify = require('browserify');
 
 module.exports = function(grunt) {
-	grunt.registerMultiTask('tape_run', 'a grunt task to run tape-run', function() {
-		var done = this.async();
+    grunt.registerMultiTask('tape_run', 'a grunt task to run tape-run', function() {
+        var done = this.async();
 
-		browserify(this.data.target)
-			.bundle()
-			.pipe(run())
-			.on('results', function() {
-				console.log;
-				done();
-			})
-			.pipe(process.stdout);
-	});
+        browserify(this.data.target)
+            .bundle()
+            .pipe(run())
+            .on('results', function(results) {
+                if (results.fail.length > 0) {
+                    grunt.fail.fatal(results.fail.length + ' test(s) have failed')
+                }
+                done();
+            })
+            .pipe(process.stdout);
+    });
 };
